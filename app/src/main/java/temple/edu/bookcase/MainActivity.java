cee692cb2a61f838cee0632abf8b963d9fb60dd4
Mike Lauw <tug01761@temple.edu>
2019-10-30 05:55:34 -0400

    adding string resources

--- app/src/main/java/temple/edu/bookcase/MainActivity.java
+++ app/src/main/java/temple/edu/bookcase/MainActivity.java
@@ -2,13 +2,24 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+    public class MainActivity extends AppCompatActivity {
+
+        List<String> books = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Resources res = getResources();
+        books = Arrays.asList(res.getStringArray(R.array.books));
+
     }
 }
