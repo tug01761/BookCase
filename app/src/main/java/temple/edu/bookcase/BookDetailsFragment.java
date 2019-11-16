c0529940bc1266d126e1e53332ab2e14c5da189e
Mike Lauw <tug01761@temple.edu>
2019-11-15 22:21:00 -0500

    fixing bookdetailsfragment and changing and adding stuffs on most of the layouts

--- app/src/main/java/temple/edu/bookcase/BookDetailsFragment.java
+++ app/src/main/java/temple/edu/bookcase/BookDetailsFragment.java
@@ -1,16 +1,25 @@
 package temple.edu.bookcase;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Objects;
 
 
 public class BookDetailsFragment extends Fragment {
-    TextView textView;
+    ConstraintLayout bookDetailsView;
+    ImageView bookCover;
+    TextView bookTitle, bookAuthor, bookPublishedIn, bookPageLength;
     public static final String BOOK_KEY = "book";
     Book book;
 
@@ -36,24 +45,41 @@ public class BookDetailsFragment extends Fragment {
             book = args.getParcelable(BOOK_KEY);
         }
     }
-/*
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        textView = (TextView) inflater.inflate(R.layout.fragment_book_details, container, false);
-        if (bookTitle != null) {
-            displayBook(bookTitle);
-        }
-        return textView;
+        bookDetailsView = (ConstraintLayout) inflater.inflate(R.layout.fragment_book_details, container, false);
+        return bookDetailsView;
     }
-    */
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        bookCover = Objects.requireNonNull(getView()).findViewById(R.id.bookCover);
+        bookTitle = Objects.requireNonNull(getView()).findViewById(R.id.bookTitle);
+        bookAuthor = getView().findViewById(R.id.bookAuthor);
+        bookPublishedIn = getView().findViewById(R.id.bookPublishedIn);
+        bookPageLength = getView().findViewById(R.id.bookPageLength);
+        if (book != null) {
+            displayBook(book);
+        }
+    }
 
     // Public method for parent Activity to "talk" to BookDetailsFragment
-    public void displayBook(String title) {
-        textView.setGravity(Gravity.CENTER);
-        textView.setText(title);
-        textView.setTextSize(30);
+    public void displayBook(Book book) {
+        bookTitle.setText(book.getTitle());
+        bookTitle.setGravity(Gravity.CENTER);
+
+        bookAuthor.setText(book.getAuthor());
+        bookAuthor.setGravity(Gravity.CENTER);
+
+        bookPublishedIn.setText(String.format(getResources().getString(R.string.publishedIn), book.getPublished()));
+        bookPublishedIn.setGravity(Gravity.CENTER);
+
+        bookPageLength.setText(String.format(getResources().getString(R.string.pageLength), book.getDuration()));
+        bookPageLength.setGravity(Gravity.CENTER);
     }
+
 }
