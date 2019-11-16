07879a08dfc30af1421f72a862792befd954772a
Mike Lauw <tug01761@temple.edu>
2019-11-15 20:19:35 -0500

    Creating Handler and fixing viewpagerfragment class

--- app/src/main/java/temple/edu/bookcase/MainActivity.java
+++ app/src/main/java/temple/edu/bookcase/MainActivity.java
@@ -4,6 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -13,12 +25,58 @@ import java.util.Arrays;
         ArrayList<Book> books = new ArrayList<>();
         boolean singlePane;
 
+        Handler booksHandler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                // Response to process from worker thread, in this case a list of books to parse
+                try {
+                    JSONArray booksArray = new JSONArray(msg.obj.toString());
+                    books = new ArrayList<>();
+                    for (int i = 0; i < booksArray.length(); i++) {
+                        // Get book at index
+                        JSONObject bookObject = booksArray.getJSONObject(i);
+                        // Create Book using JSON data
+                        Book newBook = new Book(bookObject.getInt("book_id"), bookObject.getString("title"), bookObject.getString("author"),
+                                                bookObject.getString("time"), bookObject.getInt("published"), bookObject.getString("cover_url"));
+                        // Add newBook to ArrayList<Book>
+                        books.add(newBook);
+                        Log.d("Book object from JSON: ", newBook.toString());
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return true;
+            }
+        });
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
             Resources res = getResources();
             //books.addAll(Arrays.asList(res.getStringArray(R.array.books)));
+            new Thread() {
+                @Override
+                public void run() {
+                    URL url = null;
+                    try {
+                        // Using NBA API
+                        url = new URL("https://kamorris.com/lab/audlib/booksearch.php");
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+                        StringBuilder builder = new StringBuilder(); // StringBuilder, keep adding on bits of a string
+                        String response;
+                        while ((response = reader.readLine()) != null) {
+                            builder.append(response);
+                        }
+                        // Need to use Handler
+                        Message msg = Message.obtain();
+                        msg.obj = builder.toString(); // gives you string created from StringBuilder object
+                        booksHandler.sendMessage(msg);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
 
             // Checking whether its landscape or portrait
             singlePane = (findViewById(R.id.fl_2) == null);
@@ -39,18 +97,18 @@ import java.util.Arrays;
             } else {
                 getSupportFragmentManager()
                         .beginTransaction()
-                        .replace(R.id.fl_1, BookListFragment.newInstance(books))
+                        //.replace(R.id.fl_1, BookListFragment.newInstance(books))
                         .commit();
             }
         }
 
         @Override
         public void bookSelected(int position) {
-            String bookTitle = books.get(position);
+            Book book = books.get(position);
 
             bookDetailsFragment = new BookDetailsFragment();
             Bundle detailsBundle = new Bundle();
-            detailsBundle.putString(BookDetailsFragment.BOOK_KEY, bookTitle);
+            detailsBundle.putParcelable(BookDetailsFragment.BOOK_KEY, book);
             bookDetailsFragment.setArguments(detailsBundle);
 
             if (!singlePane) {
--- app/src/main/java/temple/edu/bookcase/ViewPagerFragment.java
+++ app/src/main/java/temple/edu/bookcase/ViewPagerFragment.java
@@ -1,9 +1,8 @@
 package temple.edu.bookcase;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
@@ -14,17 +13,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class ViewPagerFragment extends Fragment {
     ViewPager myPager;
     PagerAdapter myPagerAdapter;
+    ArrayList<Book> books;
+    ArrayList<BookDetailsFragment> bookDetailsFragments;
+    public final static String BOOKS_KEY = "books";
 
     public ViewPagerFragment() {
         // Required empty public constructor
     }
 
+    public static ViewPagerFragment newInstance(ArrayList<Book> books) {
+        ViewPagerFragment viewPagerFragment = new ViewPagerFragment();
+        Bundle args = new Bundle();
+        args.putParcelableArrayList(BOOKS_KEY, books);
+        viewPagerFragment.setArguments(args);
+        return viewPagerFragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        bookDetailsFragments = new ArrayList<>();
+        if (args != null) {
+            books = args.getParcelableArrayList(BOOKS_KEY);
+            // For each book in the books ArrayList<Book>, create a BookDetailsFragment
+            if (books != null) {
+                for (int i = 0; i < books.size(); i++) {
+                    bookDetailsFragments.add(BookDetailsFragment.newInstance(books.get(i)));
+                }
+            }
+        }
     }
 
     @Override
@@ -33,37 +56,42 @@ public class ViewPagerFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_view_pager, container, false);
         myPager = view.findViewById(R.id.pager);
-        myPagerAdapter = new BookDetailsPagerAdapter(getChildFragmentManager());
+        // Instantiate a ViewPager and a PagerAdapter.
+        myPagerAdapter = new BookDetailsPagerAdapter(getChildFragmentManager(), bookDetailsFragments);
         myPager.setAdapter(myPagerAdapter);
         return view;
     }
 
-    private class BookDetailsPagerAdapter extends FragmentStatePagerAdapter {
-        BookDetailsPagerAdapter(FragmentManager fm) {
+    public ArrayList<Book> getBooks() {
+        return this.books;
+    }
+
+    private class BookDetailsPagerAdapter extends FragmentStatePagerAdapter
+    {
+        ArrayList<BookDetailsFragment> bookDetailsFragments;
+
+        BookDetailsPagerAdapter(FragmentManager fm, ArrayList<BookDetailsFragment> bookDetailsFragments)
+        {
             super(fm);
-        }
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9:
-                    return BookDetailsFragment.newInstance(getResources().getStringArray(R.array.books)[position]);
-                default:
-                    return null;
-            }
+            this.bookDetailsFragments = bookDetailsFragments;
         }
 
         @Override
-        public int getCount() {
-            return getResources().getStringArray(R.array.books).length;
+        public Fragment getItem(int position)
+        {
+            return bookDetailsFragments.get(position);
+        }
+
+        @Override
+        public int getCount()
+        {
+            return bookDetailsFragments.size();
+        }
+
+        @Override
+        public int getItemPosition(@NonNull Object object)
+        {
+            return PagerAdapter.POSITION_NONE;
         }
     }
 }
